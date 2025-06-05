8c86af09909bf044f6a89ec50de06a391fdd7173
Aakash Mahajan <mahajanaakash108@gmail.com>
2025-06-05 19:24:08 +0530

    feat: added get and post api wrappers

---
+++ client.py
@@ -0,0 +1,17 @@
+import aiohttp
+from .config import HEADERS
+
+class AngelClient:
+    def __init__(self):
+        self.headers = HEADERS
+    
+    async def get(self, url, params=None):
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url, headers=self.headers, params=params) as response:
+                return await response.json()
+    
+
+    async def post(self, url, payload):
+        async with aiohttp.ClientSession() as session:
+            async with session.post(url, headers=self.headers, json=payload) as response:
+                return await response.json()
